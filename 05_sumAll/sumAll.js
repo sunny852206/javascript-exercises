72c733eacc6abfc379014c5001b044d7ee1f5b77
sunny852206 <sunny.pslau@outlook.com>
2022-11-01 18:33:59 -0700

    passed all test cases for #05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+    return "ERROR";
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
 
+  let num = 0;
+  for (let i = a; i <= b; i++) {
+    num += i;
+  }
+  return num;
 };
 
 // Do not edit below this line
