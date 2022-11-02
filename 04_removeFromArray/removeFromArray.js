5d93af3628b5de6f4e0da3aca92b66f917969f63
sunny852206 <sunny.pslau@outlook.com>
2022-11-01 18:26:57 -0700

    passed all test cases for #04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (...arr) {
+  let target = arr[0];
+  let newArr = [];
 
+  target.forEach((item) => {
+    if (!arr.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if ((arr[i] = num)) continue;
+  //     newArr.push(arr[i]);
+  //   }
+  return newArr;
 };
 
 // Do not edit below this line
