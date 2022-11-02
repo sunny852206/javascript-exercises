691b90b20d0db886f06f18580f7462ff30ced900
sunny852206 <sunny.pslau@outlook.com>
2022-11-02 12:36:41 -0700

    passed all test cases for #9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (pstring) {
+  let string = pstring.toLowerCase().replace(/[^a-z]/g, "");
 
+  let start = 0,
+    end = string.length - 1;
+  while (start < end) {
+    if (string[start] != string[end]) return false;
+    start++;
+    end--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
