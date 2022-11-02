da4ddf9cd54e484c9cd6e9e772291b62f5ddf1a2
sunny852206 <sunny.pslau@outlook.com>
2022-11-02 14:28:04 -0700

    passed all test cases for #11 using reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let old = arr.reduce((oldestPerson, currentPerson) => {
+    let oldest = getBirth(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    let current = getBirth(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
 
+    return oldest > current ? oldestPerson : currentPerson;
+  });
+
+  return old;
+};
+
+const getBirth = function (birthYear, deathYear) {
+  if (!deathYear) {
+    deathYear = new Date().getFullYear();
+  }
+
+  return deathYear - birthYear;
 };
 
 // Do not edit below this line
